cc60f7cbea3aea5b4d2ae084ce39d4b2a570a04c
Quinn Dizon <qdizon@qdizon.fios-router.home>
2019-04-24 20:24:34 -0400

    added toggle bio type

--- src/components/about/AboutContainer.js
+++ src/components/about/AboutContainer.js
@@ -6,18 +6,40 @@ import ABOUT_CONTENT from '../../content/about';
 
 class AboutContainer extends Component {
     
-    render(){
+    constructor(props){
+        super(props);
 
-        
+        this.state = {
+            fullBio: false
+        }
+
+        this.toggleFullBio = this.toggleFullBio.bind(this);
+    }
+
+    toggleFullBio(){
+        const toggleLink = document.getElementById("toggle-full-bio")
+        const fullBio = !this.state.fullBio;
+
+        toggleLink.innerText = fullBio ? "Short Bio" : "Full Bio"; 
+
+        this.setState({fullBio});
+    }
+
+    render(){
 
         return(
             <div className="about-container">
                 
                 <TitleBox title={ABOUT_CONTENT.title} />
 
-                {renderTags(ABOUT_CONTENT.shortBio)}
-                {renderTags(ABOUT_CONTENT.fullBio)}
+                {
+                    this.state.fullBio ?
+                    renderTags(ABOUT_CONTENT.fullBio)
+                    :
+                    renderTags(ABOUT_CONTENT.shortBio)
+                }
 
+                <a id="toggle-full-bio" href="#" onClick={this.toggleFullBio}>Full Bio</a>
 
             </div>
         )
