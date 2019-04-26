081ab1a2f1686a751e009df52188ec8dd858944b
QED0711 <qdizon@gmail.com>
2019-04-26 19:36:30 -0400

    added resume content

--- src/components/resume/ResumeContainer.js
+++ src/components/resume/ResumeContainer.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
+// CONTENT
+import RESUME_CONTENT from '../../content/resume';
+
+// COMPONENTS
+import { TitleBox } from '../elements/stateless';
+
 class ResumeContainer extends Component {
     render(){
         return(
             <div className="resume-container">
-                RESUME
+                <TitleBox title={RESUME_CONTENT.title} />
             </div>
         )
     }
