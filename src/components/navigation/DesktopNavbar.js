87e6bdbcf85b91740c9c9775546d7139aa48836f
Quinn Dizon <qdizon@gmail.com>
2019-05-08 07:36:50 -0400

    added mobile nav bar

--- src/components/navigation/DesktopNavbar.js
+++ src/components/navigation/DesktopNavbar.js
@@ -6,25 +6,25 @@ class DesktopNavbar extends Component {
     render(){
         return(
             <nav className="desktop-navbar navbar">
-                <div>
+                <div className="nav-item desktop-nav-item">
                     <Link to="/">About</Link>
                 </div>
-                <div>
+                <div className="nav-item desktop-nav-item">
                     <Link to="/events">Events</Link>
                 </div>
-                <div>
+                <div className="nav-item desktop-nav-item">
                     <Link to="/listen">Listen</Link>
                 </div>
-                <div>
+                <div className="nav-item desktop-nav-item">
                     <Link to="/projects">Projects</Link>
                 </div>
-                <div>
+                <div className="nav-item desktop-nav-item">
                     <Link to="/resume">Resume</Link>
                 </div>
-                <div>
+                <div className="nav-item desktop-nav-item">
                     <Link to="/contact">Contact</Link>
                 </div>
-                <div>
+                <div className="nav-item desktop-nav-item">
                     <Link to="/blog">Blog</Link>
                 </div>
             </nav>
--- src/components/navigation/MobileNavbar.js
+++ src/components/navigation/MobileNavbar.js
@@ -1,21 +1,49 @@
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-class MobileNavbar extends Component {
 
-    render(){
-        return(
-            <nav className="mobile-navbar navbar">
-                <Link to="/">About</Link>
-                <Link to="/events">Events</Link>
-                <Link to="/listen">Listen</Link>
-                <Link to="/projects">Projects</Link>
-                <Link to="/resume">Resume</Link>
-                <Link to="/contact">Contact</Link>
-                <Link to="/blog">Blog</Link>
-            </nav>
-        )
+
+const MobileNavbar = () => {    
+    
+    let [navVisible, setNavVisible] = useState(false); 
+    const handleClick = () => {
+        const toggleDirection = !navVisible
+        setNavVisible(toggleDirection); 
     }
+    return(
+        <div className="mobile-nav-container">
+            <div className="mobile-navbar-toggle" onClick={handleClick}>
+                <div className="hamburger-icon"></div>    
+                <div className="hamburger-icon"></div>    
+                <div className="hamburger-icon"></div>    
+            </div>
+            <nav className={`mobile-navbar navbar mobile-nav-visible-${navVisible}`} onClick={handleClick}>
+                <div className="mobile-nav-item nav-item">
+                    <Link to="/">About</Link>
+                </div>
+                <div className="mobile-nav-item nav-item">
+                    <Link to="/events">Events</Link>
+                </div>
+                <div className="mobile-nav-item nav-item">
+                    <Link to="/listen">Listen</Link>
+                </div>
+                <div className="mobile-nav-item nav-item">
+                    <Link to="/projects">Projects</Link>
+                </div>
+                <div className="mobile-nav-item nav-item">
+                    <Link to="/resume">Resume</Link>
+                </div>
+                <div className="mobile-nav-item nav-item">
+                    <Link to="/contact">Contact</Link>
+                </div>
+                <div className="mobile-nav-item nav-item">
+                    <Link to="/blog">Blog</Link>
+                </div>
+                
+            </nav>
+        </div>
+    )
+    
 }
 
 export default MobileNavbar;
