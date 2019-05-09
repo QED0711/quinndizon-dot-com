ff243e17f81b0dfd0a0bade7a91fbbd5aa6ae4da
Quinn Dizon <qdizon@gmail.com>
2019-05-09 07:49:03 -0400

    added more landing style

--- src/components/landing/LandingText.js
+++ src/components/landing/LandingText.js
@@ -3,8 +3,8 @@ import React from 'react'
 const LandingText = () => {
     return(
         <div className="landing-text">
-            <h1 className="landing-title">Quinn Dizon</h1>
-            <h3 className="landing-subtitle">Composer, Programmer</h3>
+            <h1 className="landing-title">QUINN DIZON</h1>
+            <h3 className="landing-subtitle">Composer, Conductor, Programmer</h3>
         </div>
     )
 }
