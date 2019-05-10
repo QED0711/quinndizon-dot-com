5cf6775b278dfcccd3a73a0a9c0781da54758d44
Quinn Dizon <qdizon@gmail.com>
2019-05-10 08:01:45 -0400

    changed project icon enlargement from hover to click

--- src/components/projects/projectIcons.js
+++ src/components/projects/projectIcons.js
@@ -1,9 +1,14 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
 const LaptopIcon = ({ image }) => {
+    let [iconClicked, setIconClicked] = useState(false);
+    const handleClick = () => {
+        const clickToggle = !iconClicked;
+        setIconClicked(clickToggle)
+    }
     return(
-        <div className="icon-container">
+        <div className={`icon-container icon-clicked-${iconClicked}`} onClick={handleClick}>
             <div className="laptop-screen-frame icon-body">
 		        <div className="image-screen laptop-screen" style={{backgroundImage: `url("${image}")`}}></div>
 	        </div>
@@ -14,8 +19,13 @@ const LaptopIcon = ({ image }) => {
 }
 
 const TabletIcon = ({ image }) => {
+    let [iconClicked, setIconClicked] = useState(false);
+        const handleClick = () => {
+            const clickToggle = !iconClicked;
+            setIconClicked(clickToggle)
+        }
     return(
-        <div className="icon-container">
+            <div className={`icon-container icon-clicked-${iconClicked}`} onClick={handleClick}>
             <div className="tablet-screen-frame icon-body">
                 <div className="image-screen tablet-screen" style={{backgroundImage: `url("${image}")`}}></div>
                 <div className="tablet-button"></div>
@@ -25,8 +35,13 @@ const TabletIcon = ({ image }) => {
 }
 
 const MobileIcon = ({ image }) => {
+    let [iconClicked, setIconClicked] = useState(false);
+    const handleClick = () => {
+        const clickToggle = !iconClicked;
+        setIconClicked(clickToggle)
+    }
     return(
-        <div className="icon-container">
+        <div className={`icon-container icon-clicked-${iconClicked}`} onClick={handleClick}>
             <div className="mobile-screen-frame icon-body" >
                 <div className="image-screen" style={{backgroundImage: `url("${image}")`}}></div>
             </div>
