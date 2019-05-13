be7f07dacba6fd15b2be8631cb530bc1085a9a7c
QED0711 <qdizon@gmail.com>
2019-05-13 18:54:51 -0400

    added more test content to blog post

--- src/content/blogPosts/binaryHarmony.js
+++ src/content/blogPosts/binaryHarmony.js
@@ -19,7 +19,32 @@ import {
      "An introduction to my theories and use of binary harmony",
      
      heading(1, "Overview"),
-     paragraph("There are many ways we can construct harmonic structures."),
+     paragraph(
+        "I have always been fascinated with the ways music and technology can be combined.",
+        "As a composer, I am particulary interesting in see how technology can be used to augment the process working with and conceiving of musical materials.",
+        "To this end, my recent work with Limited Pitch-Class Interval Sequences (LIPCS), and in particular, binary harmony, has yielded interesting results." 
+      ),
+      paragraph(
+          "In this post, which I hope will be the first of a few posts on this topic, will cover the basics of these theories I developed to handle binary, ternary, and quaternary LPCISs."
+      ),
+      heading(2, "TL;DR (or musical set theory isn't my bag)"),
+      paragraph(
+          'The abridged version for those who haven\'t studied non-tonal theory is this.',
+          'We can think of collections of pitches in terms of what pitch classes they contain (A,C,D, etc.).',
+          "We can also think about them in terms of how those pitch classes are related to each other (intervals - the sapce between two pitches/pitch classes).",
+          "When we put pitch-classes in a specified order"
+      ),
+      htmlInsert(<hr/>),
+      heading(2, "What is a LIPCS"),
+      paragraph(
+          "A LIPCS - Limited Pitch Class Interval Sequence - is a way to think about ordering chains of pitch-class intervals.",
+          'The term, "Limited" comes in when we predetermine which pitch-class intervals we use in our orderings.',
+      ),
+      paragraph(
+          'Let\'s consider an example where we choose two PCIs, 2 and 3, represented as a set, (2,3).',
+          'We can think of this as a binary set'
+      ),
+      heading(2, "Modeling Harmonic Possibilitiese with LIPCSs"),
      divImage("https://cdn.pixabay.com/photo/2016/04/13/15/04/music-sheet-1327003_960_720.jpg", {backgroundSize: "contain", width: "100%", height: "12em", margin: "0 auto", }),
      orderedList("Designate a binary set", "Map all sequences of 0 to the set size")
 
