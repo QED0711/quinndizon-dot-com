f23a85039aa0a2c85dd16ef23b9ab47687c5c3c9
Quinn Dizon <qdizon@gmail.com>
2019-05-15 06:35:06 -0400

    changed card color

--- src/js/tags.js
+++ src/js/tags.js
@@ -106,7 +106,7 @@ const listenInfoBox = (title, subtitle, imageUrl, scoreSrc, scTrackId) => {
 
 const soundcloudFrame = (trackId) => {
     return(
-        <iframe width="100%" height="auto" scrolling="no" frameBorder="no" allow="autoplay" src={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${trackId}&amp;color=%23fad8bf&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=false&amp;show_reposts=false&amp;show_teaser=true&amp;visual=false&amp;show_artwork=false`}></iframe>
+        <iframe width="100%" height="auto" scrolling="no" frameBorder="no" allow="autoplay" src={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/${trackId}&amp;color=%23EAE4D5&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=false&amp;show_reposts=false&amp;show_teaser=true&amp;visual=false&amp;show_artwork=false`}></iframe>
     )
 }
 
