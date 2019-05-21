6f87d4c4cd358efe97d66f2da92de46fa304c2de
QED0711 <qdizon@gmail.com>
2019-05-21 15:14:02 -0400

    added content to projects

--- src/content/projects.js
+++ src/content/projects.js
@@ -19,6 +19,8 @@ import scorestudyDesktop from '../media/projects/scorestudy/desktop.JPG'
 import scorestudyTablet from '../media/projects/scorestudy/tablet.JPG'
 import scorestudyMobile from '../media/projects/scorestudy/mobile.JPG'
 
+import cssComponentManagerDesktop from '../media/projects/cssComponentManager/Desktop.JPG'
+
 const PROJECTS_CONTENT = {
 
     title: "Projects",
@@ -35,7 +37,7 @@ const PROJECTS_CONTENT = {
                 heading(3, "Technologies"),
                 unorderedList("React", "Noteflight Client API", "gh-pages"),
                 htmlInsert(<hr/>),
-                heading(3, "About"),
+                heading(3, "Features"),
                 unorderedList(
                     "Front-end application that allows instructors to turn any Noteflight score into a self grading music theory assignment",
                     "Instructors can share specially generated links to their assignments with an unlimited number of assignments allowed",
@@ -70,7 +72,7 @@ const PROJECTS_CONTENT = {
                 heading(3, "Technologies"),
                 unorderedList("Bootstrap", "JQuery + Ajax", "gh-pages"),
                 htmlInsert(<hr/>),
-                heading(3, "About"),
+                heading(3, "Features"),
                 unorderedList(
                     "A fullstack application that allows users to designate composers, time periods, and genres of music theory would like to study.",
                     "Currates targeted study examples from more than twenty-thousand scores scraped from online databases like IMSLP.org",
@@ -89,53 +91,78 @@ const PROJECTS_CONTENT = {
         // ==========================================================================================================
 
         projectInfoBox(
+            "CSS Component Manager",
+            [
+                heading(2, "CSS Component Manger", {textAlign: 'center'}), 
+                heading(5, "2019", {textAlign: 'center', margin: 0}), 
+                paragraph(
+                    "An electron desktop application that allows users to create and manage large libraries of css styles, and compile them down to single files for deployment"
+                ),
+                htmlInsert(<hr/>),
+                heading(3, "Technologies"),
+                unorderedList("JS + Electron"),
+                htmlInsert(<hr/>),
+                heading(3, "Features"),
+                unorderedList(
+                    "Desktop application built using Electron, HTML, and Javascript",
+                    "The application helps to better modularize the css component creation process by allowing user to break up their code into dedicated projects",
+                    "Within each project, users can write HTML and CSS code for specific components, and see the results displayed in real time in a dummy window",
+                    "When users are finished writing their components, they can compile the CSS code to a single file on their machine, and deploy it in any other project",
+                ),
+            ],
+            "https://github.com/QED0711/css-component-creator",
+            null,
+            cssComponentManagerDesktop,
+            null,
+            null,
+            "Video"
+        ),
+
+        // ==========================================================================================================
+        
+        projectInfoBox(
             "Git Checklist",
             [
                 heading(2, "Git Checklist", {textAlign: 'center'}), 
-                orderedList("An electron desktop application to help users manage track and manage incremental changes in their git workflow", "provides a simple UI to checkoff tasks and commit them to the current branch"),
+                heading(5, "2019", {textAlign: 'center', margin: 0}), 
+                paragraph(
+                    "An electron desktop application to help users plan and manage their git workflow from a simple UI. Helps to encourage simple task completion and regular commits."
+                ),
+                htmlInsert(<hr/>),
+                heading(3, "Technologies"),
+                unorderedList("JS + Electron"),
+                htmlInsert(<hr/>),
+                heading(3, "Features"),
+                unorderedList(
+                    "Users can point the application to any git initialized project. If there is no .gitchecklist file present, it will create one.",
+                    "In a project's .gitchecklist file, users can store and track the completion of project goals",
+                    "In the UI, users can checkoff completed tasks and have commits automatically generated for each completed itam"
+                ),
             ],
-            "https://github.com",
-            null,
-            "https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/E6xpS0jFxik7ugje7/videoblocks-hi-tech-user-interface-head-up-display-with-digital-floating-information-for-background-computer-desktop-screen-display_h-k0x5hfm_thumbnail-full01.png",
+            "https://github.com/QED0711/git-checklist",
             null,
             null,
-            // "https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/E6xpS0jFxik7ugje7/videoblocks-hi-tech-user-interface-head-up-display-with-digital-floating-information-for-background-computer-desktop-screen-display_h-k0x5hfm_thumbnail-full01.png",
-            "Video"
-        ),
-       
-        // ==========================================================================================================
-       
-        projectInfoBox(
-            "CSS Compiler",
-            [
-                heading(2, "CSS Compiler", {textAlign: 'center'}), 
-                orderedList("A CSS component designer toolkit built in electron", "provides a simple UI to checkoff tasks and commit them to the current branch"),
-            ],
-            "https://github.com",
-            null,
-            "https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/E6xpS0jFxik7ugje7/videoblocks-hi-tech-user-interface-head-up-display-with-digital-floating-information-for-background-computer-desktop-screen-display_h-k0x5hfm_thumbnail-full01.png",
             null,
             null,
-            // "https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/E6xpS0jFxik7ugje7/videoblocks-hi-tech-user-interface-head-up-display-with-digital-floating-information-for-background-computer-desktop-screen-display_h-k0x5hfm_thumbnail-full01.png",
             "Video"
         ),
 
         // ==========================================================================================================
 
-        projectInfoBox(
-            "Personal Website",
-            [
-                heading(2, "Personal Website", {textAlign: 'center'}),
-                paragraph("This is a paragraph about this project", "This is a second sentence about this project"),
-                unorderedList("Here is an list item", "here is another"),
-            ],
-            null,
-            "http://quinndizon.com",
-            "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
-            "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
-            "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
-            "Video"
-        ),
+        // projectInfoBox(
+        //     "Personal Website",
+        //     [
+        //         heading(2, "Personal Website", {textAlign: 'center'}),
+        //         paragraph("This is a paragraph about this project", "This is a second sentence about this project"),
+        //         unorderedList("Here is an list item", "here is another"),
+        //     ],
+        //     null,
+        //     "http://quinndizon.com",
+        //     "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+        //     "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+        //     "https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+        //     "Video"
+        // ),
     ]
 
 }
