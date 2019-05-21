e97d4ab60b6edc226e1f16a2a3aff47116b89226
Quinn Dizon <qdizon@gmail.com>
2019-05-20 20:16:47 -0400

    added headshot and style to about page

--- src/components/about/BioBox.js
+++ src/components/about/BioBox.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { TitleBox } from '../elements/stateless';
 import renderContent from '../../js/renderConent';
 
+import headshot from '../../media/headshot_med.jpg';
+
 class BioBox extends Component {
     constructor(props){
         super(props);
@@ -25,7 +27,8 @@ class BioBox extends Component {
     render(){
         const { content } = this.props
         return(
-            <div className="bio-box">
+            <div className="inner-content-body bio-box">
+                <img className='headshot-image' src={headshot} alt="headshot" />
                 {this.state.fullBio ? renderContent(content.fullBio) : renderContent(content.shortBio)}
                 <p className="bio-version"><a id="toggle-full-bio" href="#" onClick={this.toggleFullBio}>Full Bio</a></p>
             </div>
