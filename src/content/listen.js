df25df4a666bdcfb991c9be8ee57f892ca1ed507
Quinn Dizon <qdizon@gmail.com>
2019-05-21 20:23:10 -0400

    added works to listen page

--- src/content/listen.js
+++ src/content/listen.js
@@ -10,9 +10,10 @@ const LISTEN_CONTENT = {
     title: "Listen",
 
     works : [
+
         listenInfoBox(
             "Dark Nebula",
-            "for wind orchestra",
+            "for Wind Orchestra",
             "https://www.nasa.gov/sites/default/files/styles/full_width_feature/public/thumbnails/image/heic1517a_0.jpg",
             "https://drive.google.com/file/d/1a9yL3g9sFoHMdZm2iCqPtG44H_hVAwKy/preview",
             546384945,
@@ -25,11 +26,51 @@ const LISTEN_CONTENT = {
             223397327,
         ),
         listenInfoBox(
+            "Awakening",
+            "for piano quartet",
+            "https://images.pexels.com/photos/1252869/pexels-photo-1252869.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+            "https://drive.google.com/file/d/0B8TgLmq2pHPvSGF4QU1fWXVRcGs/preview",
+            223519244,
+        ),
+        listenInfoBox(
+            "12 Haiku",
+            "an orchestral song cycle",
+            "https://images.pexels.com/photos/46253/mt-fuji-sea-of-clouds-sunrise-46253.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+            "https://drive.google.com/file/d/0B8TgLmq2pHPvamh1UWg4TDdoczA/preview",
+            "Audio Coming Soon",
+            {backgroundPositionY: "-200px"}
+        ),
+        listenInfoBox(
+            "Concertante",
+            "for orchestra",
+            "https://images.pexels.com/photos/355863/pexels-photo-355863.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+            "https://drive.google.com/file/d/0B8TgLmq2pHPveHdSSU5NZk8xMk0/preview",
+            239482583,
+            {backgroundPositionY: "-700px"}
+        ),
+        listenInfoBox(
+            "String Qurartet No. 1",
+            "with live electronics",
+            "http://imgsrc.hubblesite.org/hvi/uploads/image_file/image_attachment/29082/large_web.jpg",
+            "https://drive.google.com/file/d/0B8TgLmq2pHPvVEtMRVRuQ1BWN1U/preview",
+            303732634,
+            {backgroundPositionY: "33%"}
+        ),
+        listenInfoBox(
+            "Grace",
+            "for orchestra & chorus",
+            "https://images.pexels.com/photos/189349/pexels-photo-189349.jpeg?auto=compress&cs=tinysrgb&h=350",
+            "https://drive.google.com/file/d/0B8TgLmq2pHPvV2swRU15QzVvWlk/preview",
+            223523435,
+            {backgroundPositionY: "-150px"}
+        ),
+        listenInfoBox(
             "Prelude",
             "for solo cello",
-            "https://images.pexels.com/photos/158163/clouds-cloudporn-weather-lookup-158163.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
-            "https://drive.google.com/file/d/1LHcdy2Rcc7xCdg-RpGcpdBAuIwE_k-xA/preview",
-            223397327,
+            "https://images.pexels.com/photos/261184/pexels-photo-261184.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+            "https://drive.google.com/file/d/0B8TgLmq2pHPvWmRBa3NGM1BEMXc/preview",
+            223397328,
+            {backgroundPositionY: "-150px"}
         ),
     ]
 
