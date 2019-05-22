c32849ef2ae67360f380af5837df114e029966c7
Quinn Dizon <qdizon@gmail.com>
2019-05-21 20:11:58 -0400

    added image background to listen info boxes

--- src/components/listen/ListenInfoBox.js
+++ src/components/listen/ListenInfoBox.js
@@ -12,9 +12,11 @@ const ListenInfoBox = ({ work, setCurrentWork }) => {
     return(
 
         <Link to={`/listen/${parseTitle(work.title)}`} onClick={setCurrentWork(work)} className="content-selector-link">
-            <div className="listen-info-box content-card content-selector-card">
-                <h3>{work.title}</h3>
-                <h5>{work.subtitle}</h5>
+            <div className="listen-info-box content-card " style={{backgroundImage: `url(${work.imageUrl})`}}>
+                <div className="listen-info-box-image-cover content-selector-card">
+                    <h3>{work.title}</h3>
+                    <h5>{work.subtitle}</h5>
+                </div>
             </div>
         </Link>
 
