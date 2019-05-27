688d86ec8c229fb10d4da4609543e50851ae7cbb
QED0711 <qdizon@gmail.com>
2019-05-27 10:41:19 -0400

    added content to n harmony blog post

--- src/content/blogPosts/binaryHarmony.js
+++ src/content/blogPosts/binaryHarmony.js
@@ -34,13 +34,15 @@ export default postBox(
      "An introduction to my theories and use of N Harmonic Structures",
      
     heading(2, "Overview"),
-    paragraph("In this post, I will introduce the concepts of Limited Pitch-Class Interval Sequences, and discuss how and why I developed this approach to harmonic organization."),  
-    paragraph("Note: this post assumes some basic knowledge of musical set-theory. "),
+    paragraph("In this brief post, I will introduce the concepts of N Hrmmonic Sctructures, and discuss how and why I developed this approach to harmonic organization."),  
+    htmlInsert(<p><em><strong>Note:</strong> this post assumes some basic knowledge of musical set-theory.</em></p>),
     
+    htmlInsert(<hr/>),
     
     paragraph(
         "When I compose, music is about drama.",
-        "Though drama can take many forms in music, and be affected by many different elements (rhythm, melody, etc.), I typically find myself obsessing over how the drama will play out harmonically.",
+        "Though drama can take many forms in music, and be affected by many different elements (rhythm, melody, etc.), I typically find myself obsessing over how the drama will be realized harmonically.",
+        "For this reason, developing a strong harmonic foundation for a particular piece is crucial for my compositional process.",    
     ),
     paragraph(
         "But harmony in the modern era can be a bit of a double-edged sword.",
@@ -59,10 +61,17 @@ export default postBox(
     paragraph("Example: the set, {2,3,5} contains PCIs 2, 3, and 5.", "These need not be in sorted order, but I will often show them in this way.", {marginLeft: "1em"}),
     
     htmlInsert(<em><p><strong>Limited Pitch-Class Interval Sequence (LPCIS): </strong>An ordered sequence of pitch classes in which each dyadic adjacency forms one of the pitch-class intervals of an interval set.</p></em>),
-    paragraph("Example:", {marginLeft: "1em"}),
+    paragraph("Example: Set Class 5-34[02469] (realized here starting on PC 4) can be arranged in such a way so that it forms a LPCIS with PC Intervals 2 and 3.", {marginLeft: "1em"}),
     image(lpcisExample, "LPCIS <4 6 9 0 2>", imageTagStyle),
+    paragraph(
+        "In the actual application of LPCISs in composition, I typically realize them as continuously ascending or continuously descending sequences of pitches in pitch-space.", 
+        "This is not a criteria for a LPCIS, but rather a means of focusing on one particular subcategory of LPCISs.",
+        "This will all be discussed in more detail in Part 2 of this series.",
+        {marginLeft: "1em"}
+    ),
+    
     htmlInsert(<em><p><strong>N Harmony: </strong>A set class associated with a LIPCS</p></em>),
-    paragraph("Example: ", "The SC, 5-34[02469] is associated with the LIPCS shown above.", {marginLeft: "1em"}),
+    paragraph("Example: ", "The SC, 5-34[02469] is associated with the LPCIS built from the interval set, {2,3}.", {marginLeft: "1em"}),
     
     htmlInsert(
         <p>
@@ -88,13 +97,26 @@ export default postBox(
     paragraph(
         "At the end of the day, N Harmony is just another avenue into set theory.",
         "We are, after all, working with pitch-class sets and set classes once the LPCISs have been formed.",
-        "What N Harmony provides is an alternative method for mapping relationships within and among pitch-class sets.",
+        "What N Harmony provides is an alternative method for mapping relationships within and among pitch class sets.",
     ),
 
     paragraph(
-        "As you might logically assume, a single set-class may be associated with more than one LPCISs.",
-    )
+        "As you might logically assume, a single set class may be associated with more than one LPCIS.",
+        "And from a given interval set, we can generate several (millions!) unique LPCISs that correspond to a vast array of set classes.",
+    ),
+    paragraph(
+        "So the true power of N Harmony is twofold.",
+        "On one hand, it can be used to help us understand the relationships among different set classes.",
+        "But more interesting (at least to me as a composer), is that for any interval set, we can generate a unique N Harmonic Network.",
+        "Once we have a harmonic network from which to work, we can devise our own ways of moving through the harmonic nodes of these networks, and even hoping to new networks via pivot nodes (nodes that belong to multiple networks). ",
+    ),
+    paragraph(
+        "In essence, N Harmony provides us with both a means of quantifying relationships among set classes, and an open roadmap from which to explore those relationships musically."
+    ),
 
+    paragraph(
+        "This may seem a bit cryptic now, but check back in for Part 2 of this series where I demonstrate how N Harmony can be used as a generative tool in composition."
+    ),
     // codeGist({id: "911e00f20e7328c0a892a75fc6dcaec0"}),
 
 
